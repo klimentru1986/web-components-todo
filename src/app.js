da6c7d894342cddf97ccd4db7a0c66ccfd6a301f
klimentru1986 <kliment1986@inbox.ru>
2019-01-04 09:55:53 +0300

    webpack config

--- src/app.js
+++ src/app.js
@@ -1,79 +1,79 @@
-(function() {
-  const MOCK_TODOS = [
-    { id: 1, name: 'Выучить Vue', completed: false },
-    { id: 2, name: 'Выучить React', completed: false },
-    { id: 3, name: 'Выучить Angular', completed: true },
-    { id: 4, name: 'Выучить Web Components', completed: false }
-  ];
+import './styles.css';
 
-  class ToDoApp {
-    constructor() {
-      this._todos = [...MOCK_TODOS];
-      this._addTodo = document.querySelector('#add-todo');
-      this._todoList = document.querySelector('#todo-list');
+const MOCK_TODOS = [
+  { id: 1, name: 'Выучить Vue', completed: false },
+  { id: 2, name: 'Выучить React', completed: false },
+  { id: 3, name: 'Выучить Angular', completed: true },
+  { id: 4, name: 'Выучить Web Components', completed: false }
+];
 
-      this._addTodoEventListener();
-      this._deleteTodoEventListener();
-      this._toggleCompleted();
+class ToDoApp {
+  constructor() {
+    this._todos = [...MOCK_TODOS];
+    this._addTodo = document.querySelector('#add-todo');
+    this._todoList = document.querySelector('#todo-list');
 
-      this._renderTodoList();
-    }
+    this._addTodoEventListener();
+    this._deleteTodoEventListener();
+    this._toggleCompleted();
 
-    _addTodoEventListener() {
-      this._addTodo.addEventListener('addTodo', ev => {
-        if (!this._addTodo.value) {
-          return;
-        }
+    this._renderTodoList();
+  }
 
-        const todo = {
-          id: this._getTodoId(),
-          name: this._addTodo.value,
-          completed: false
-        };
+  _addTodoEventListener() {
+    this._addTodo.addEventListener('addTodo', ev => {
+      if (!this._addTodo.value) {
+        return;
+      }
 
-        this._todos = [todo, ...this._todos];
-        this._todoList.innerHTML =
-          this._mapTodoToElement(todo) + this._todoList.innerHTML;
-        this._addTodo.value = null;
-      });
-    }
+      const todo = {
+        id: this._getTodoId(),
+        name: this._addTodo.value,
+        completed: false
+      };
 
-    _toggleCompleted() {
-      this._todoList.addEventListener('toggleCompleted', ev => {
-        const element = ev.target;
-        const todo = this._todos.find(td => td.id === +element.id);
-        todo.completed = !todo.completed;
-        element.setAttribute('completed', todo.completed);
-      });
-    }
+      this._todos = [todo, ...this._todos];
+      this._todoList.innerHTML =
+        this._mapTodoToElement(todo) + this._todoList.innerHTML;
+      this._addTodo.value = null;
+    });
+  }
 
-    _deleteTodoEventListener() {
-      this._todoList.addEventListener('deleteTodo', ev => {
-        const element = ev.target;
-        this._todos = this._todos.filter(td => td.id !== +element.id);
-        this._todoList.removeChild(element);
-      });
-    }
+  _toggleCompleted() {
+    this._todoList.addEventListener('toggleCompleted', ev => {
+      const element = ev.target;
+      const todo = this._todos.find(td => td.id === +element.id);
+      todo.completed = !todo.completed;
+      element.setAttribute('completed', todo.completed);
+    });
+  }
 
-    _renderTodoList() {
-      const todos = this._todos.map(td => this._mapTodoToElement(td));
-      this._todoList.innerHTML = [...todos].reverse().join('');
-    }
+  _deleteTodoEventListener() {
+    this._todoList.addEventListener('deleteTodo', ev => {
+      const element = ev.target;
+      this._todos = this._todos.filter(td => td.id !== +element.id);
+      this._todoList.removeChild(element);
+    });
+  }
 
-    _mapTodoToElement(todo) {
-      return `<todo-item id="${todo.id}" 
+  _renderTodoList() {
+    const todos = this._todos.map(td => this._mapTodoToElement(td));
+    this._todoList.innerHTML = [...todos].reverse().join('');
+  }
+
+  _mapTodoToElement(todo) {
+    return `<todo-item id="${todo.id}" 
             name="${todo.name}" 
             completed="${todo.completed}">
             ${todo.name}
           </todo-item>`;
-    }
-
-    _getTodoId() {
-      const id = this._todos[0] ? this._todos[0].id : 0;
-
-      return id + 1;
-    }
   }
 
-  new ToDoApp();
-})();
+  _getTodoId() {
+    const id = this._todos[0] ? this._todos[0].id : 0;
+
+    return id + 1;
+  }
+}
+
+new ToDoApp();
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/app.js',
     components: './src/components/components.js'
   },
-  mode: 'development',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({ use: ['css-loader'] })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   }
 };
